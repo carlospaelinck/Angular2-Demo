c6ab215a28b1639dc432c13e12a9ba9f0fcf649f
Carlos Paelinck <carlos@carlospaelinck.com>
2016-06-16 19:12:35 -0500

    Added styling for cart.

--- app/components/cart/item/cart.item.ts
+++ app/components/cart/item/cart.item.ts
@@ -2,12 +2,13 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 
 @Component({
   selector: 'cart-item',
-  template: require('./cart.item.html')
+  template: require('./cart.item.html'),
+  styles: [ require('./cart.item.scss') ]
 })
 
 export class CartItemComponent {
   @Input() pizza: Object
-  @Output() onRemove: EventEmitter<any> = new EventEmitter()
+  @Output() onRemove: EventEmitter<Object> = new EventEmitter()
 
   removeFromCart() {
     this.onRemove.emit(this.pizza)
